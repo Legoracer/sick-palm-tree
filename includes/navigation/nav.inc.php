0230afae92022f5cfe61b1ba472ac1bcde19a840
Legoracer <maxivanec5@gmail.com>
2021-04-29 13:02:05 +0200

    Work on welcome screen

--- includes/navigation/nav.inc.php
+++ includes/navigation/nav.inc.php
@@ -1,11 +1,9 @@
 <?php 
     $pages = array(
         "index.php" => "Home",
-        "customize.php" => "Customize",
-        "menu.php" => "Menu",
-        "deals.php" => "Deals",
-        "about.php" => "About us",
-        "cart.php" => "Cart"
+        "brands.php" => "Brands",
+        "watches.php" => "Watches",
+        "about.php" => "About"
     );
 
     function isActive($linkUrl){
@@ -15,19 +13,24 @@
     }
 ?>
 
-<nav class="navbar navbar-expand-lg navbar-light">
+
+<nav class="navbar navbar-expand-lg navbar-light border-bottom">
+<a href="./index.php">
+    <img src="./images/logo.png" width="70px" height="60px" alt="logo">
+</a>
   <button class="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
+
   <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav ml-auto">
+    <ul class="navbar-nav mx-auto">
         <?php
             foreach($pages as $url => $title){
                 $isActive = isActive($url);
 
                 $link = "
-                <li class='nav-item active mr-5 ml-4 mt-2'>
-                    <a class='nav-link' href='$url'>
+                <li class='nav-item active mx-5 mt-2'>
+                    <a class='nav-link " . ($isActive ? "font-weight-bold " : "") . "' href='$url'>
                         $title 
                     </a>
                 </li>
@@ -36,7 +39,6 @@
             }
         ?>
     </ul>   
-
   </div>
 </nav>
 
--- index.php
+++ index.php
@@ -8,16 +8,22 @@
     <title>Homepage</title>
     <?php require $metaHtml; ?>
     <?php require $bsCss; ?>
-    <link rel="stylesheet" href="css/style.css">
   </head>
-  <body>
+  <body> 
     <?php require $navigation; ?>
 
-    <div>
-        <!-- <h1 class="index-customize"> CUSTOMIZE </h1> -->
-        <img src="images/ingredients2.jpeg" class="ingredients-image img-fluid mt-5">
+    <div class="video-text">
+      <img class="logo-video" src="images/logo.png" width="100px" height="90px" alt="logo">
+      <video id="vid" autoplay loop muted>
+          <source src="./images/video.mp4" type="video/mp4">
+        Your browser does not support the video tag.
+      </video>
+      <p class="text-video text-light">Limited Addiction</p> <br>
+      <p class="text-light text-video1">The obsession is real. Discover watches with limited production and endless demand.</p>
     </div>
-    
+    <script>document.getElementById('vid').play();</script> <!-- woo hoo we love web design -->
+
     <?php require $bsScript; ?>
+    <?php require "includes/footer.inc.php";?>
   </body>
 </html>
